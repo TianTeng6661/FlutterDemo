77460cc1cd50883d01f393da5918a34b965b0b7d
TianTeng <tianteng6661@163.com>
2020-12-29 03:04:29 +0800

    feat(Flutter):模仿微信通讯录页面

--- lib/pages/mine_page.dart
+++ lib/pages/mine_page.dart
@@ -9,7 +9,6 @@ class MinePage extends StatefulWidget {
 }
 
 class _MinePageState extends State<MinePage> {
-
   Widget headerWidget() {
     return Container(
       color: Colors.white,
@@ -19,52 +18,57 @@ class _MinePageState extends State<MinePage> {
         margin: EdgeInsets.only(top: 100, bottom: 20),
 //        padding: EdgeInsets.all(10),
         child: Container(
-          margin: EdgeInsets.only(left: 30),
+          margin: EdgeInsets.only(left: 20),
 //          padding: EdgeInsets.all(5),
 //          color: Colors.red,
           child: Row(
             children: <Widget>[
               Container(
-                width: 50,
-                height: 50,
+                width: 70,
+                height: 70,
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+//                  color: Colors.blue,
                   borderRadius: BorderRadius.circular(10.0),
                   image: DecorationImage(
-                      image: AssetImage('images/微信表情.png'), fit: BoxFit.cover),
+                      image: AssetImage('images/Hank.png'), fit: BoxFit.cover),
                 ),
               ), //头像
-              Container(
-                margin:
-                EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 5),
-                width: MediaQuery.of(context).size.width - 95,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      child: Text(
-                        'TT_Yunxiao',
-                        style: TextStyle(fontSize: 25, color: Colors.black),
-                      ),
-                    ), //昵称
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            '微信号:TT_Yunxiao',
-                            style: TextStyle(fontSize: 14, color: Colors.grey),
-                          ),
-                          Image(
-                            image: AssetImage('images/icon_right.png'),
-                            width: 15,
-                          )
-                        ],
-                      ),
-                    ), //微信号+箭头
-                  ],
-                ),
-              ), //右边的部分
+              Expanded(
+                child: Container(
+//                  margin: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 5),
+//                  width: MediaQuery.of(context).size.width - 95,
+                  padding: EdgeInsets.only(left: 10, top: 8, right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 35,
+                        child: Text(
+                          'Hank',
+                          style: TextStyle(fontSize: 25, color: Colors.black),
+                        ),
+                      ), //昵称
+                      Container(
+                        height: 35,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              '微信号:1234',
+                              style:
+                              TextStyle(fontSize: 17, color: Colors.grey),
+                            ),
+                            Image(
+                              image: AssetImage('images/icon_right.png'),
+                              width: 15,
+                            )
+                          ],
+                        ),
+                      ), //微信号+箭头
+                    ],
+                  ),
+                ), //右边的部分,
+              )
             ],
           ),
         ),
@@ -87,18 +91,55 @@ class _MinePageState extends State<MinePage> {
                   children: <Widget>[
                     headerWidget(), //头部
                     SizedBox(
-                      height: 10,
+                      height: 7,
                     ),
                     DiscoverCell(
                       imageName: 'images/微信 支付.png',
                       title: '支付',
                     ),
                     SizedBox(
-                      height: 10,
+                      height: 7,
                     ),
                     DiscoverCell(
                       imageName: 'images/微信收藏.png',
                       title: '收藏',
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(width: 50, height: 0.5, color: Colors.white),
+                        Container(height: 0.5, color: Colors.grey)
+                      ],
+                    ), //分割线
+                    DiscoverCell(
+                      imageName: 'images/微信相册.png',
+                      title: '相册',
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(width: 50, height: 0.5, color: Colors.white),
+                        Container(height: 0.5, color: Colors.grey)
+                      ],
+                    ), //分割线
+                    DiscoverCell(
+                      imageName: 'images/微信卡包.png',
+                      title: '卡包',
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(width: 50, height: 0.5, color: Colors.white),
+                        Container(height: 0.5, color: Colors.grey)
+                      ],
+                    ), //分割线
+                    DiscoverCell(
+                      imageName: 'images/微信表情.png',
+                      title: '表情',
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    DiscoverCell(
+                      imageName: 'images/微信设置.png',
+                      title: '设置',
                     ),
                   ],
                 ),
@@ -106,7 +147,7 @@ class _MinePageState extends State<MinePage> {
           Container(
             height: 25,
             color: Color.fromRGBO(0, 0, 0, 0),
-            margin: EdgeInsets.only(top: 40, right: 15),
+            margin: EdgeInsets.only(top: 40, right: 10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
@@ -120,6 +161,5 @@ class _MinePageState extends State<MinePage> {
       ),
     );
   }
-
 }
 
